656cb7f14d8a84218f56eb7b57b4c2d2ffbaee6b
yuxuanx <xiayuxuan1993@outlook.com>
2018-04-20 12:01:50 +0200

    minor changes

--- Project2/id2tf.m
+++ Project2/id2tf.m
@@ -6,11 +6,11 @@ switch(m.model)
     case 'ARX'
         A = [1 m.theta(1:m.n(1))'];
         B = [zeros(1,m.n(3)) m.theta(m.n(1)+1:end)'];
-        tfsys = tf(A,B,1,'Variable','q^-1');
+        tfsys = tf(B,A,1,'Variable','q^-1');
     case 'OE'
         F = [1 m.theta(1:m.n(1))'];
         B = [zeros(1,m.n(3)) m.theta(m.n(1)+1:end)'];
-        tfsys = tf(F,B,1,'Variable','q^-1');
+        tfsys = tf(B,F,1,'Variable','q^-1');
 end
 
 end
--- Project2/idcompare.m
+++ Project2/idcompare.m
@@ -1,4 +1,4 @@
-function idcompare(z,model,horizon)
+function [prediction, simulation, groundTruth] = idcompare(z,model,horizon)
 %Plot function, compare the simulated/predicted output together with the
 % true output with uncertainty region
 
@@ -6,13 +6,13 @@ prediction = idpredict(model,z,horizon);
 
 simulation = idsimulate(model,z);
 
-trueOutput = z(1:length(z)/2);
+groundTruth = z(1:length(z)/2);
 
 figure
 hold on
-plot(trueOutput,'Linewidth',2);
-plot(prediction,'Linewidth',2);
-plot(simulation,'Linewidth',2);
+plot(groundTruth,'Linewidth',2);
+plot(prediction,'--','Linewidth',2);
+plot(simulation,'-.','Linewidth',2);
 str = strcat('Predicted output, K=', num2str(horizon));
 xlabel('Time step'); ylabel('Output')
 legend('True output',str,'Simulated Output')
